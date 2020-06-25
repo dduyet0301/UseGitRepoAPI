f5c99e1bc1ff823f6de814ba43e960e86a9ae5c1
gem <gem@DoDucDuyet.local>
2020-06-25 16:54:01 +0700

    add proj

--- UseGithubAPI/ViewController.swift
+++ UseGithubAPI/ViewController.swift
@@ -67,17 +67,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-//        let lastSectionIndex = tableView.numberOfSections - 1
-//        let lastRowIndex = tableView.numberOfRows(inSection: lastSectionIndex) - 1
-//        if indexPath.section ==  lastSectionIndex && indexPath.row == lastRowIndex {
-//            // print("this is the last cell")
-//            let spinner = UIActivityIndicatorView(style: .gray)
-//            spinner.startAnimating()
-//            spinner.frame = CGRect(x: CGFloat(0), y: CGFloat(0), width: tableView.bounds.width, height: CGFloat(44))
-//
-//            self.tableInfo.tableFooterView = spinner
-//            self.tableInfo.tableFooterView?.isHidden = false
-//        }
+        let lastSectionIndex = tableView.numberOfSections - 1
+        let lastRowIndex = tableView.numberOfRows(inSection: lastSectionIndex) - 1
+        if indexPath.section ==  lastSectionIndex && indexPath.row == lastRowIndex {
+            let spinner = UIActivityIndicatorView(style: .gray)
+            spinner.startAnimating()
+            spinner.frame = CGRect(x: CGFloat(0), y: CGFloat(0), width: tableView.bounds.width, height: CGFloat(44))
+
+            self.tableInfo.tableFooterView = spinner
+            self.tableInfo.tableFooterView?.isHidden = false
+        }
         if indexPath.row == Contains.arrRepo.count - 1 {
             if Contains.arrRepo.count < 500 {
                 var index = Contains.arrRepo.count
